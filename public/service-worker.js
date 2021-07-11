45dc4d0dcae18b24e55d9e7543d4651216ea5950
Ross White <rosswhite@outlook.com>
2021-07-11 12:02:34 +0100

    Adding activate to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,6 +20,24 @@ self.addEventListener('install', event => {
     self.skipWaiting();
 });
 
+// activate
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
 self.addEventListener("fetch", function (event) {
     if (event.request.url.includes("/api/")) {
         event.respondWith(
