75d188852a209da0b0721f9a82eb21fb7bcea453
Ross White <rosswhite@outlook.com>
2021-07-11 11:42:08 +0100

    FIX: Fixing service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -43,35 +43,17 @@ self.addEventListener("fetch", function (event) {
         return;
     }
 
-    self.addEventListener("fetch", function (evt) {
-        if (evt.request.url.includes("/api/")) {
-            evt.respondWith(
-                caches.open(DATA_CACHE_NAME).then(cache => {
-                    return fetch(evt.request)
-                        .then(response => {
-                            // If the response was good, clone it and store it in the cache.
-                            if (response.status === 200) {
-                                cache.put(evt.request.url, response.clone());
-                            }
 
-                            return response;
-                        })
-                        .catch(err => {
-                            // Network request failed, try to get it from the cache.
-                            return cache.match(evt.request);
-                        });
-                }).catch(err => console.log(err))
-            );
-
-            return;
-        }
-
-        event.respondWith(
-            caches.open(CACHE_NAME).then(cache => {
-                return cache.match(event.request).then(response => {
-                    return response || fetch(event.request);
-                });
-            })
-        );
-    })
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else if (event.request.headers.get("accept").includes("text/html")) {
+                    // return the cached home page for all requests for html pages
+                    return caches.match("/");
+                }
+            });
+        })
+    );
 });
